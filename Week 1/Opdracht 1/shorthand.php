eb51e0c59fbcbf62617ddb95497e764f2c0c7c4e
BerkayAtakli <9023046@student.zadkine.nl>
2026-03-02 15:08:13 +0100

    Initial commit

---
+++ Week 1/Opdracht 1/shorthand.php
@@ -0,0 +1,24 @@
+<?php
+
+$naam = "Berkay";
+$leeftijd = "20";
+$favoriete_programmeertaal = "JavaScript";
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opdracht 1</title>
+</head>
+<body>
+    <p>Mijn naam is <?= $naam ?></p>
+    <p>Mijn leeftijd is <?= $leeftijd ?></p>
+    <p>Mijn favoriete programmeertaal is <?= $favoriete_programmeertaal ?></p>
+    
+</body>
+</html>
